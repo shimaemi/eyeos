e6f53219da7d17682ad421eaeef7dd8222e9786d
emi shimada <eshimada@csus.edu>
2024-10-23 12:58:46 -0700

    .

--- i2c_luna_test.py
+++ i2c_luna_test.py
@@ -2,7 +2,7 @@ import smbus
 import time
 
 bus = smbus.SMBus(1) # Change the I2C bus number based on the actual device
-
+time.sleep(1)
 address = 0x10 # Radar default address 0x10
 getLidarDataCmd = [0x5A,0x05,0x00,0x01,0x60] # Gets the distance value instruction
 
