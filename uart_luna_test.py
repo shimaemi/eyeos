628cd8dc3d89b4b28263ae129d95dbbbacbbf502
emi shimada <eshimada@csus.edu>
2024-10-27 15:48:38 -0700

    Update uart_luna_test.py

--- uart_luna_test.py
+++ uart_luna_test.py
@@ -15,15 +15,15 @@ def read_data():
             if bytes_serial[0] == 0x59 and bytes_serial[1] == 0x59: # python3
                 distance = bytes_serial[2] + bytes_serial[3]*256 # centimeters
                 # strength = bytes_serial[4] + bytes_serial[5]*256 # Amp value over 32768 indicates ambient light overexposure
-                if distance < 50 and range < 2
+                if distance < 50 and range < 2:
                     print("Warning: within 50 cm")
                     range = 2
-                elif distance < 100 and distance > 50 and range < 1
+                elif distance < 100 and distance > 50 and range < 1:
                     print("Warning: within 100 cm")
                     range = 1
-                elif distance > 100 and range > 0
+                elif distance > 100 and range > 0:
                     range = 0
-                elif distance > 50 and distance < 100 and range > 1
+                elif distance > 50 and distance < 100 and range > 1:
                     range = 1
                 # print("Strength:" + str(strength))
                 ser.reset_input_buffer()
