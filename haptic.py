8c2ce8cfaf614fb46b9bde1d4818ce75ae3c5021
emi <eshimada@csus.edu>
2024-11-20 11:49:31 -0800

    .

---
+++ haptic.py
@@ -0,0 +1,35 @@
+import serial # uart
+import time
+import RPi.GPIO as GPIO
+import gpiozero import PWMOutputDevice
+
+# Initialize the haptic sensor on a specific GPIO pin
+haptic_sensor = PWMOutputDevice(17)  # Replace 17 with the GPIO pin you're using
+
+def vibrate():
+    haptic_sensor.on()
+    sleep(.1)
+    haptic_sensor.off()
+
+def vibrate2():
+    haptic_sensor.on()
+    sleep(.1)
+    haptic_sensor.off()
+    sleep(.1)
+    haptic_sensor.on()
+    sleep(.1)
+    haptic_sensor.off()
+                
+if __name__ == "__main__":
+    try:
+        while(1):
+            print("warning 1")
+            vibrate()
+            sleep(2)
+            print("warning 2")
+            vibrate2()
+            sleep(2)
+
+    except KeyboardInterrupt:
+            GPIO.cleanup()
+            print("program interrupted by the user")
