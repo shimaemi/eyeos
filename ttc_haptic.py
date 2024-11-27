a26f8dc0a8b70b0f41f51387458d1c3e42ba26c9
Midy Voong <125234661+LightAurora@users.noreply.github.com>
2024-11-27 04:37:39 -0800

    test

--- ttc_haptic.py
+++ ttc_haptic.py
@@ -8,12 +8,15 @@ lgpio.gpio_claim_output(h, 18)
 
 try:
     while True:
-        for duty_cycle in range(0, 101, 10):
-            lgpio.tx_pwm(h, 18, 100, duty_cycle)
-            time.sleep(1)
-        for duty_cycle in range(0, -1, -10):
-            lgpio.tx_pwm(h, 18, 100, duty_cycle)
-            time.sleep(1)
+        ttc_value = sensor.print_ttc()
+        if ttc_value is not None:
+            if ttc_value <= 5:
+                lgpio.tx_pwm(h, 18, 1000, 1000000)  # Strong vibration
+            elif ttc_value <= 10:
+                lgpio.tx_pwm(h, 18, 500, 1000000)  # Medium vibration
+            else:
+                lgpio.tx_pwm(h, 18, 0, 0)  # No vibration
+
 except KeyboardInterrupt:
     pass
 finally:
