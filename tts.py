3d1db73654d870f0884af5ed91aa5714e1d74ec2
emi <eshimada@csus.edu>
2025-02-25 09:12:28 -0800

    .

---
+++ tts.py
@@ -0,0 +1,26 @@
+# pip install pyttsx3
+#  sudo apt update && sudo apt install espeak-ng libespeak1
+
+import bluetooth
+import pyttsx3
+import time
+
+# specify the MAC address of the device you want to connect to
+target_device = "41:42:64:07:20:BB"
+
+# pair with the device
+bluetooth.pair(target_device)
+
+subprocess.run(['bluetoothctl', 'connect', target_device])
+time.sleep(1)
+
+engine = pyttsx3.init() # object creation
+engine.setProperty('rate', 125)     # setting up new voice rate
+engine.setProperty('volume',1.0)    # setting up volume level  between 0 and 1
+engine.setProperty('voice', voices[1].id)   #changing index, changes voices. 1 for female
+
+engine.say("warning")
+time.sleep(1)
+engine.say("WARNING")
+engine.runAndWait()
+engine.stop()
