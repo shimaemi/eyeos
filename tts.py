54f930e3dc54583014a58df04c9abe7ae6f21cf6
emi <eshimada@csus.edu>
2025-03-18 14:16:38 -0700

    .

--- tts.py
+++ tts.py
@@ -6,15 +6,6 @@ import pyttsx3
 import time
 
 if __name__ == "__main__":
-    # specify the MAC address of the device you want to connect to
-    target_device = "41:42:64:07:20:BB"
-
-    # pair with the device
-    bluetooth.pair(target_device)
-
-    subprocess.run(['bluetoothctl', 'connect', target_device])
-    time.sleep(1)
-
     engine = pyttsx3.init() # object creation
     engine.setProperty('rate', 125)     # setting up new voice rate
     engine.setProperty('volume',1.0)    # setting up volume level  between 0 and 1
