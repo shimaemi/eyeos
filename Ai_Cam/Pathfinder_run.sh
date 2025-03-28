0b8ada2110ba8b91e2f8b9d5a9d8e84e79a58d69
LightAurora <voongmidy@gmail.com>
2025-03-27 21:33:33 -0700

    Changed Script

--- Ai_Cam/Pathfinder_run.sh
+++ Ai_Cam/Pathfinder_run.sh
@@ -1,1 +1,1 @@
-python Pathfinder_Start.py --model /usr/share/imx500-models/imx500_network_ssd_mobilenetv2_fpnlite_320x320_pp.rpk
+python Pathfinder_Start.py --model /usr/share/imx500-models/imx500_network_yolov8n_pp.rpk
